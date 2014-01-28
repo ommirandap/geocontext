9b8da32e3c111a8e10a00584af91e62590f42ca4
ommirandap <omiranda@dcc.uchile.cl>
2014-01-28 17:25:16 -0300

    Changes on the constructor

--- remote-src/database/GeoPoint.py
+++ remote-src/database/GeoPoint.py
@@ -1,24 +1,45 @@
 import json, re
 
 class GeoPoint(object):
-    """ This class was made for give a representation for GeoPoints, acording to the GeoJSON information (Due to the discondance on using [Long,Lat] or [Lat,Long]"""
+    """
+    This class was made for give a representation for GeoPoints, 
+    according to the GeoJSON information 
+    (Due to the differences on using [Long,Lat] or [Lat,Long]).
+    """
     
-    defaultPoint = 0.0 
-
     def __init__(self, constructor=()):
-        self.longitude  = constructor[0]
-        self.latitude   = constructor[1]
+        if constructor == None:
+            self.longitude = 0.0
+            self.latitude  = 0.0
+        else:
+            self.longitude  = constructor[0]
+            self.latitude   = constructor[1]
 
     def __str__(self):
         """Method docstring."""
         return str(self.longitude)+ "," + str(self.latitude)
     
     @classmethod
-    def from_string(cls, str):
+    def initFromString(cls, stringLikeCoord, LatLongStyle = False):
+        """
+        Calls the constructor of GeoPoint, giving an array of [Long,Lat] 
+        from a string with the same form. 
+        If the string it's of the [Lat, Long] (like DataScienceToolkit API), 
+        then LatLongStyle must be set as True.
+        """
         regx = re.compile('(.+)(\,)(.+)')
-        match = regx.search(str)
+        match = regx.search(stringLikeCoord)
         if match:
-            constr = [float(regx.search(str).group(1)),float(regx.search(str).group(3))]
+            if LatLongStyle:
+                constr = [
+                    float(regx.search(stringLikeCoord).group(3)),
+                    float(regx.search(stringLikeCoord).group(1))
+                    ]
+            else:
+                constr = [
+                    float(regx.search(stringLikeCoord).group(1)),
+                    float(regx.search(stringLikeCoord).group(3))
+                    ]                
         else:
             constr = None
         return cls(constr)
@@ -38,13 +59,6 @@ class GeoPoint(object):
         points[1] = self.latitude
         return points
 
-    def toArrayForDSTK(self):
-        """ Return the object as an array [Longitude, Latitude] """
-        points = [0.0, 0.0]
-        points[0] = self.latitude
-        points[1] = self.longitude
-        return points
-
     def toGeoJSON(self):
         """ """
         defaultDict = {"type": "Point"}
@@ -59,11 +73,20 @@ class GeoPoint(object):
     	""" Returns the Longitude of the point """
     	return self.longitude
 
+    def setLatitude(self, latitude):
+        """ Returns the Latitude of the point """
+        self.latitude = latitude
 
+    def setLongitude(self, longitude):
+        """ Returns the Longitude of the point """
+        self.longitude = longitude
+
+
+"""
 def main():
 
     a = GeoPoint.from_values(23.14123,-82.12343)
-    a = GeoPoint.from_string("23.14123 -82.12343")
+    a = GeoPoint.__init__from_string("23.14123 -82.12343")
     print str(a)
     ble = json.loads(a.toGeoJSON())
     print ble["type"]
@@ -73,3 +96,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
