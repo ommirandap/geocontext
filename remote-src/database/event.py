9fec2cde97d9ea1ce50c90255daf7892558cc174
ommirandap <omiranda@dcc.uchile.cl>
2014-01-29 14:51:52 -0300

    Some docstrings added

--- remote-src/database/event.py
+++ remote-src/database/event.py
@@ -3,26 +3,35 @@ from datetime import datetime
 FORMAT_DATETIME = "%Y-%m-%d %H:%M:%S"
 
 class Event(object):
-    """ An event is identified by 3 fields; his ID, a couple of keywords (1 string) and a datetime. This schema comes from the DB and this was created only to manipulate those records easily"""
-
+    
     def __init__(self, eventID, keywords, datetime):
-        """Method docstring."""
+        """
+        An event is identified by 3 fields: his ID, a couple of keywords 
+        (one string) and a datetime. 
+        This schema comes from the DB and this was created only to manipulate 
+        those records easily.
+        """
         self.eventID = eventID
         self.keywords = keywords
         self.datetime = datetime
 
     def __str__(self):
-    	"""Method docstring."""
-    	return str(self.eventID)+ " - " + str(self.keywords) + " - " + str(self.datetime.strftime(FORMAT_DATETIME))
+    	"""Dump the event as a string."""
+    	return (str(self.eventID)+ " - " 
+                + str(self.keywords) + " - " 
+                + str(self.datetime.strftime(FORMAT_DATETIME)))
     
     def getID(self):
-        """ Return the ID of the event (Long) """
+        """
+        Return the ID of the eventID.
+        Type: long.
+        """
         return self.eventID
 
     def getKeywords(self):
-    	""" Returns the couple of keywords of the Event (1 string) """
+    	"""Returns the couple of keywords of the Event (1 string)."""
     	return self.keywords
 
     def getDatetime(self):
-    	""" Returns the datetime when the Event was added to the DB (datetime) """
+    	"""Returns the datetime when the Event was added to the DB (datetime)."""
     	return self.datetime
