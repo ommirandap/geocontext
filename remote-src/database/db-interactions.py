c53bcb301cc3622d6f6b8dbc30f71583ff21b38a
ommirandap <omiranda@dcc.uchile.cl>
2014-01-31 15:00:41 -0300

    ...

--- remote-src/database/db-interactions.py
+++ remote-src/database/db-interactions.py
@@ -7,11 +7,10 @@ from GeoPoint import GeoPoint
 import CountryCodes as CD
 import LocationCleaner
 
-# >
-# <
 FORMAT_DATETIME = "%Y-%m-%d %H:%M:%S"
 Q_LAST_EVENT 			= "SELECT datetime FROM event ORDER BY id DESC LIMIT 1"
-Q_EVENTS_BETWEEN 		= "SELECT id, keywords, datetime FROM event WHERE datetime >= %s AND datetime <= %s"
+Q_EVENTS_BETWEEN 		= """SELECT id, keywords, datetime FROM event 
+							WHERE datetime >= %s AND datetime <= %s"""
 Q_TWEETS_ID_FROM_EVENT 	= "SELECT tweet_id FROM tweet WHERE event_id_id = "
 Q_USERS_ID_FROM_EVENT 	= "SELECT user_id_id FROM tweet WHERE event_id_id = "
 Q_LOCATION_FROM_USER 	= "SELECT location FROM user WHERE user_id = "
@@ -19,17 +18,24 @@ Q_COORD_FROM_TWEET 		= "SELECT coordinates FROM tweet WHERE tweet_id = "
 global lastEventDT
 global Count_Dict
 
-""" -------------------------- Funciones ------------------------------- """
-def queryToDB(cursor, query):
+def getConnection():
+	"""
+	Returns a connection object whom will be given to any DB Query function.
+	"""
 	try:
-		cursor.execute(query)
-		return cursor.fetchall()
-	except MySQLdb.Error:
-		print "Error: unable to fetch data"
-		return -1
+		connection = MySQLdb.connect(k.MYSQLDB_HOST, k.MYSQLDB_USER, 
+							k.MYSQLDB_KEY, k.MYSQLDB_NAME)
+		return connection	
+	except MySQLdb.DatabaseError, e:
+		print 'Error %s' % e    
+		sys.exit(1)
 
-def getLastEventDateTime(cursor):
+def getLastEventDateTime(connection):
+	"""
+	Returns a string with the Datetime of the lastEvent registered on the DB.
+	"""
 	try:
+		cursor = connection.cursor()
 		cursor.execute(Q_LAST_EVENT)
 		data = cursor.fetchone()
 		return datetime.strptime(str(data[0]), FORMAT_DATETIME)
@@ -37,45 +43,70 @@ def getLastEventDateTime(cursor):
 		print "Error: unable to fetch data"
 		return -1
 
-def getEventsBetweenTwoDateTimes(cursor):
-	lastEventDT = getLastEventDateTime(cursor)
+def getTodayEvents(connection):
+	"""
+	Returns all the events (as an Array of Events Objects) added to the DB, 
+	between the 00:00hrs and the time of the last event added (on the same day).
+	"""
+	lastEventDT = getLastEventDateTime(connection)
 	if lastEventDT != -1:
 		try:
-			zeroTodayDT = datetime(lastEventDT.year, lastEventDT.month, lastEventDT.day, 0, 0, 0)
-			cursor.execute(Q_EVENTS_BETWEEN, (zeroTodayDT.strftime(FORMAT_DATETIME), lastEventDT.strftime(FORMAT_DATETIME),)) 
-			return cursor.fetchall()
+			zeroTodayDT = datetime(lastEventDT.year, lastEventDT.month, 
+									lastEventDT.day, 0, 0, 0)
+			cursor = connection.cursor()
+			cursor.execute(Q_EVENTS_BETWEEN, 
+							(zeroTodayDT.strftime(FORMAT_DATETIME), 
+							lastEventDT.strftime(FORMAT_DATETIME),)) 
+			internalEvents = []
+			for row in cursor.fetchall():
+				event = Event(row[0], row[1], row[2])
+				internalEvents.append(event)
+			return internalEvents
 		except MySQLdb.Error:
 			print "Error: unable to fetch data"
 			return -1
+	else:
+		print "Something went wrong with getting the last event of today"
+		sys.exit(1)
 
-def getTweetsIDByEvent(cursor, ID_Event):
+def getTweetsIDByEvent(connection, ID_Event):
+	"""
+	Returns all the ID of the Tweets from an specified EventID.
+	"""
 	try:
+		cursor = connection.cursor()
 		cursor.execute(Q_TWEETS_ID_FROM_EVENT + str(ID_Event))
 		return cursor.fetchall()
 	except MySQLdb.Error:
 		print "Error: unable to fetch data"
 		return -1
 
-def getUsersIDByEvent(cursor, ID_Event):
+def getUsersIDByEvent(connection, ID_Event):
+	"""
+	Returns all the 
+	"""
 	try:
+		cursor = connection.cursor()
 		cursor.execute(Q_USERS_ID_FROM_EVENT + str(ID_Event))
 		return cursor.fetchall()
 	except MySQLdb.Error:
 		print "Error: unable to fetch data"
 		return -1
 
-def getLocationFromUserID(cursor, ID_User):
+def getLocationFromUserID(connection, ID_User):
 	try:
+		cursor = connection.cursor()
 		cursor.execute(Q_LOCATION_FROM_USER + str(ID_User))
 		return cursor.fetchone()
 	except MySQLdb.Error:
 		print "Error: unable to fetch data"
 		return -1
 
-def getCoordinatesFromTweetID(cursor, ID_tweet):
+def getCoordinatesFromTweetID(connection, ID_tweet):
 	try:
+		cursor = connection.cursor()
 		cursor.execute(Q_COORD_FROM_TWEET + str(ID_tweet))
-		result = (cursor.fetchone())[0]
+		result = (connection.fetchone())[0]
 		if result == "None":
 			return None
 		else:
@@ -84,28 +115,25 @@ def getCoordinatesFromTweetID(cursor, ID_tweet):
 			if match:	
 				return extractCordRegex.search(result).group(3)
 			else:
-				# Due to that Twitter ALWAYS return the same when the tweet has a Location, this code should never be reached
+				#Due to that Twitter ALWAYS return the same when the tweet has
+				#a Location, this code should never be reached.
 				pass
 
 	except MySQLdb.Error:
 		print "Error: unable to fetch data"
 		return -1
-#TODO
-def saveTweetIDAndLocation(cursor, IDTweet, Location):
-	"""
-	Take an IDTweet with a Location Object and record it to the Local DB.
-	"""
-	pass
 
-def getLocationFromDSTK(cursor, anEvent, Count_Dict):
-	
+def getLocationFromDSTK(connection, anEvent, Count_Dict):
+	"""
+	Deprecated, since we use now a local DB for match the closer GeoCoordenate.
+	"""
 	List_Tweets_Coordenatesful  = []
 	Dict_Tweets_Coordenatesful 	= {}
 	List_Tweets_Coordenatesless = []
 
-	List_Tweets = getTweetsIDByEvent(cursor, anEvent.getID())
-	
+	List_Tweets = getTweetsIDByEvent(connection, anEvent.getID())
 	for tweetID in List_Tweets:
+		cursor = connection.cursor()
 		probableCoord = getCoordinatesFromTweetID(cursor, tweetID[0])
 		if probableCoord == None:
 			List_Tweets_Coordenatesless.append(tweetID[0])
@@ -117,7 +145,8 @@ def getLocationFromDSTK(cursor, anEvent, Count_Dict):
 	dstk = DSTK()
 	for ID in Dict_Tweets_Coordenatesful.keys():
 		geoPointFromID = Dict_Tweets_Coordenatesful[ID]
-		geoPointForQuery = [geoPointFromID.getLatitude(), geoPointFromID.getLongitude()]
+		geoPointForQuery = [geoPointFromID.getLatitude(), 
+							geoPointFromID.getLongitude()]
 		coordinates_dstk = dstk.coordinates2politics(geoPointForQuery)
 		if coordinates_dstk[0]["politics"] is not None:
 			result = coordinates_dstk[0]["politics"][0]["name"]
@@ -132,33 +161,25 @@ def getLocationFromDSTK(cursor, anEvent, Count_Dict):
 			Count_Dict[code] = 1 
 		
 		print "Event: %s\t analizing: %s \t from = %s code = %s" % (anEvent.getKeywords(), str(geoPointForQuery), result, code) 
-	
-
-
-""" ----------------------------------------------------------------------- """
 
 def main():
-	db = MySQLdb.connect(k.MYSQLDB_HOST, k.MYSQLDB_USER, 
-							k.MYSQLDB_KEY, k.MYSQLDB_NAME)
-	cursor = db.cursor()
+	connection = getConnection()
+	
 	Array_ID_events = []
 	Dict_ID_Keywords = {}
 	Array_Events = []
 
 	Count_Dict = {}
-	for row in getEventsBetweenTwoDateTimes(cursor):
-		event = Event(row[0], row[1], row[2])
-		Array_Events.append(event)
-		#Array_ID_events.append(row[0])
-		#Dict_ID_Keywords[row[0]] = row[1]
+
+	Array_Events = getTodayEvents(connection)
 	
 	for everyEvent in Array_Events:
-		#getLocationFromDSTK(cursor, everyEvent, Count_Dict)
+		#getLocationFromDSTK(connection, everyEvent, Count_Dict)
 
 		# ID de usuarios que twitearon en el evento[0]
-		List_Users = getUsersIDByEvent(cursor, everyEvent.getID())
+		List_Users = getUsersIDByEvent(connection, everyEvent.getID())
 		for user in List_Users:
-			locationOfUser = getLocationFromUserID(cursor, user[0])
+			locationOfUser = getLocationFromUserID(connection, user[0])
 			effectiveLoc = locationOfUser[0]
 			cleanedLoc = LocationCleaner.cleanLocationField(effectiveLoc)
 			if type(cleanedLoc) == str:
@@ -167,8 +188,7 @@ def main():
 				print "Input: %s \t Result GeoPoint: %s" % (effectiveLoc, str(cleanedLoc))
 			if cleanedLoc == None:
 				print "Input: Useless = %s \t Result None" % effectiveLoc
-	db.close()
-
+	connection.close()
 
 if __name__ == "__main__":
 	main()
